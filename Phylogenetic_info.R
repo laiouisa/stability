c707543c624928fe231f2412686a3b8732a0fca9
Luisa Conti <luisa.conti@gmail.com>
2022-10-19 22:14:50 +0200

    reviewed models with categorical traits, added file with phylogenetic iinfo in the main models

--- Phylogenetic_info.R
+++ Phylogenetic_info.R
@@ -9,12 +9,12 @@ library(ape) #plot.phylo
 library(phylobase)
 
 
-#data from original analyses
+#data from original analyses-----
 # load("~/OneDrive - ČZU v Praze/STABILITY Project/Traits/workspace.RData")#nope?
 mean_stability_all<-as_tibble(read.csv("mean_stability_all.csv"))
 
 
-#load phylo data
+#load phylo data------
 load("~/OneDrive - ČZU v Praze/STABILITY Project/Traits/Luisa_tree.RData")
 summary(Luisa_tree)
 head(Luisa_tree)
@@ -28,7 +28,7 @@ length(Luisa_tree$tip.label)
 mean_stability_all$Group.1
 
 
-#convert tree format
+#convert tree format-----
 Luisa_tree<-as(Luisa_tree, "phylo4") 
 class(Luisa_tree) #phylo4
 str(Luisa_tree)
@@ -56,12 +56,85 @@ mean_stability_all[grep("Lycopodium", mean_stability_all$Group.1), "growthform"]
 #Subspecies were turned to spp in table
 #Ferns are missing from tree
 
-#subset tree
+#subset tree-----
 length(Luisa_tree@label[Luisa_tree@label%in%unique(mean_stability_all$Group.1)])
 sub_tree<-phylobase::subset(Luisa_tree, tips.include=Luisa_tree@label[Luisa_tree@label%in%unique(mean_stability_all$Group.1)])
 treePlot(sub_tree, show.tip.label = F, type = "fan") #we need a better plot - circular and small label characters
 
 save(sub_tree, file="subset_tree.RData")
+
+#--------Using eigenvalues (PCA axes of phylogenetic distance)------------
+
+load("dc.phylo.eig.r") 
+head(dc.phylo.eig) #?? are these from the db RDA??
+dim(dc.phylo.eig) #50 axes for #461 observations??
+summary(is.na(dc.phylo.eig))
+load("phylo.eig.r")
+head(phylo.eig)
+dim(phylo.eig) #15 axes for 1750 observations
+names(phylo.eig)
+row.names(phylo.eig)
+summary(is.na(phylo.eig))
+
+eigenval<-phylo.eig
+eigenval$names<- row.names(eigenval)
+summary(is.na(eigenval))
+
+#add all eigenvalues (15 axes selected from the 50 that explain 80% of variability in the phylogeny) to main table 
+
+mean_stability_all_phylo<-left_join(mean_stability_all,eigenval, by=c('Group.1'='names'))
+mean_stability_all_phylo$A50
+mean_stability_all_phylo[c('Group.1', 'A1')]
+names(mean_stability_all_phylo)
+
+#Option 1 - Effects of traits beyond phylogeny - using residuals of a model of CV explained by eigenvalues
+names(mean_stability_all_phylo)
+mod_phylo<- lm(as.formula(paste('z.sp_cv', paste(names(mean_stability_all_phylo)[43:57], collapse=" + "), sep=" ~ ")), 
+                 data=mean_stability_all_phylo) #the model doesn't include random effects
+#careful with - 15 rows missing values in z.sp_cv, -71 rows missing values in eigenvalues 
+#original dim 3884 observations, actual dim 3798
+summary(mod_phylo)
+dim(mod_phylo$model)  
+
+#IT ALL WORKS BETTER IF I USE LESS AXES (StepAIC on the 15 axes..)
+
+#using spp as random effect
+mod_phylo$terms
+mod_phylo_random<- lmer(z.sp_cv ~ A1+ A4+A7+A10+A14+A19+A23+A24+A25+A31+A33+A36+A38+A43+A50+ 
+                                (1|Group.1)+(1|Group.2), data=mean_stability_all_phylo) 
+summary(mod_phylo_random) #Number of obs: 3798, groups:  Group.1, 1745; Group.2, 78
+
+#make residual table with species names 
+phylo_residuals<-as.tibble(resid(mod_phylo_random))
+phylo_residuals$species<-mean_stability_all_phylo$Group.1[!is.na(z.sp_cv)&!is.na(mean_stability_all_phylo['A1'])]
+unique(phylo_residuals$species) #1745 species
+#add residuals to phylo table
+mean_stability_all_phylo<-left_join(mean_stability_all_phylo, phylo_residuals, by=c('Group.1'='species'))
+colnames(mean_stability_all_phylo)[58]<-'phylo_residuals'
+
+#model using residuals i.e. portion of the CV variability left unexplained by the phylogeny
+names(mean_stability_all_phylo)
+mod_cv_phyloresid<- lmer(phylo_residuals ~ z.log.mean_height + z.mean_LeafN + z.mean_LeafP +    
+                           z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + z.mean_SSD +
+                           (1|Group.1) + (1|Group.2), data=mean_stability_all_phylo)
+#boundary (singular) fit: see ?isSingular
+summary(mod_cv_phyloresid) #Number of obs: 8595, groups:  Group.1, 92; Group.2, 67
+coefplot(mod_cv_phyloresid)
+r.squaredGLMM(mod_cv_phyloresid) #R2m 0.001610543 R2c 0.007688418
+
+mod_cv_phyloresid_final<- lmer(phylo_residuals ~  z.mean_LeafN + z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC +
+                           (1|Group.1)+ (1|Group.2) , data=mean_stability_all_phylo)
+# boundary (singular) fit: see ?isSingular
+summary(mod_cv_phyloresid_final) #Number of obs: 14124, groups:  Group.1, 390; Group.2, 77
+rePCA(mod_cv_phyloresid_final) #it is beccause of datasets 
+coefplot(mod_cv_phyloresid_final)
+r.squaredGLMM(mod_cv_phyloresid_final)#R2m 0.00423158 R2c 0.01560563
+
+mod_cv_phyloresid_final.1<- lmer(phylo_residuals ~  z.mean_LeafN + z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC +
+                                 (1|Group.1) , data=mean_stability_all_phylo) #no singularity
+summary(mod_cv_phyloresid_final.1) #Number of obs: 14124, groups:  Group.1, 390
+coefplot(mod_cv_phyloresid_final.1)
+r.squaredGLMM(mod_cv_phyloresid_final.1) #R2m 0.00423158 R2c 0.01560564 - it stays the same!! 
 
 
 
--- categorical_traits.R
+++ categorical_traits.R
@@ -8,18 +8,27 @@ library(lme4)#Mixed Effects models
 library(MuMIn) #r.squared + confidence intervals with visreg (replaces predict.merMod?)
 library(car)
 library(coefplot)
+library(jtools)
+library(huxtable)
 
 #data-------
 #data from original analyses
 # load("~/OneDrive - ČZU v Praze/STABILITY Project/Traits/workspace.RData")#nope?
 mean_stability_all<-as_tibble(read.csv("mean_stability_all.csv"))
 names(mean_stability_all)
+dim(mean_stability_all)
+# observations: 3884 (species x plot)
+length(unique(mean_stability_all$Group.1)) #1799 species
+
+summary(is.na(mean_stability_all$z.sp_cv)) #15 entries with no standardized CV
+mean_stability_all[is.na(mean_stability_all$z.sp_cv),c("Group.1", "Group.2","sp_cv" , "residency_prop", "z.sp_cv")]
 
 #Categorical traits names
 # "growthform"  "woodyness" "woodyness_new" "lifeform" "lifespan" "lifespan_new"    
 
 #check categorical trait models
 
+attach(mean_stability_all)
 
 #WOODYNESS------------
 summary(as.factor(mean_stability_all$woodyness))
@@ -27,14 +36,34 @@ summary(as.factor(mean_stability_all$woodyness))
 # 3271             126             487 
 summary(is.na(mean_stability_all$woodyness)) #no NAs
 summary(as.factor(mean_stability_all$woodyness_new))
-
+length(unique(mean_stability_all$Group.1[mean_stability_all$woodyness_new=="woody"]))
+# non-woody                woody 
+# 3397  (1494 species)     487 (305 species)
+summary(is.na(mean_stability_all$woodyness_new)) #no NAs
 
 mod_wood<-lmer(z.sp_cv~woodyness_new  -1 +
                  (1|Group.1) + (1|Group.2),
                mean_stability_all, REML=F)
 summary(mod_wood)
 r.squaredGLMM(mod_wood)
+#               R2m      R2c
+# [1,] 7.035184e-07 0.228913
 coefplot(mod_wood)
+
+# source("/Users/laiouisa/OneDrive - ČZU v Praze/R/R scripts/facetzoom2.R") #for facet zoom on the right 
+
+# est<-coefplot(mod_wood)$data
+# est$Coefficient<- as.factor(c("non-woody", "woody"))
+m_wood<- ggplot(data = mean_stability_all[!is.na(mean_stability_all$woodyness_new)&!is.na(mean_stability_all$z.sp_cv),], 
+                aes(y=z.sp_cv, x=woodyness_new, color=lifespan_new))+
+  geom_jitter(size=0.5)+ #colour="gray", 
+  geom_violin(colour="black", position="identity", fill=NA ) +
+  # geom_point(data=est, mapping = aes(y=Value, x=Coefficient), color="red", size=3)+
+  # geom_errorbar(data=est, mapping = aes( x=Coefficient, ymin = LowOuter, ymax = HighOuter) , color="red", size=0.5, inherit.aes = F, width = 0)+
+  # geom_errorbar(data=est, mapping = aes( x=Coefficient, ymin = LowInner, ymax = HighInner), color="red", size=1.5, inherit.aes = F, width = 0)+
+  # facet_zoom(ylim = c(-0.1, 0.2), zoom.size = 1, zoom.data = z.sp_cv==0) #facet zoom on the right
+  theme_classic(base_size=15) + xlab(label = "") +ylab(label = "species CV") +  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+m_wood
 
 
 mod_wood1<-lmer(z.sp_cv~woodyness  -1 +
@@ -44,11 +73,13 @@ summary(mod_wood1)
 r.squaredGLMM(mod_wood1)
 coefplot(mod_wood1)
 
-#non-woody/woody species have a highly variable CV!
+#non-woody/woody species have a highly variable CV! - focusing on non-woody species
 
 
 #LIFE FORM------------
-summary(as.factor(mean_stability_all$lifeform)) #1381 NAs
+summary(as.factor(mean_stability_all$lifeform)) #1381/3884 NAs - 806/1799 species
+summary(as.factor(woodyness_new[is.na(lifeform)]))
+length(unique(Group.1[is.na(lifeform)]))
 mod_lifeform<-lmer(z.sp_cv~lifeform -1 +  
                      (1|Group.1) + (1|Group.2),
                    mean_stability_all, REML=F)
@@ -60,6 +91,7 @@ r.squaredGLMM(mod_lifeform)
 #LIFE SPAN----------
 summary(as.factor(mean_stability_all$lifespan)) #1143 NAs (715 spp that were defined as not-annual)
 summary(as.factor(mean_stability_all$lifespan_new)) #no Nas?
+length(unique(Group.1[lifespan_new=='annual']))
 # annual             not-annual 
 # 606  (283 spp)     3278 (1516 spp)
 
@@ -73,6 +105,29 @@ mod_lifespan<-lmer(z.sp_cv~lifespan_new -1 +
 summary(mod_lifespan)
 coefplot(mod_lifespan)
 r.squaredGLMM(mod_lifespan)
+
+#lifespan and other traits
+mean_stability_all[mean_stability_all$lifespan_new=='annual',] #606 observations 
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.sp_cv"] ) #No NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.log.mean_height"] ) #9 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.mean_LeafN"] ) #343 NAs !!
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.mean_LeafP"] ) #372 NAs !!
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.log.mean_SeedMass"] ) #23 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.log.mean_SLA"] ) #115 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.mean_LDMC"] ) #199 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='annual',"z.mean_SSD"] ) #562 NAs !!
+
+#lifespan and other traits
+mean_stability_all[mean_stability_all$lifespan_new=='not-annual',] #3278 observations 
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.sp_cv"] ) #15 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.log.mean_height"] ) #1050 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.mean_LeafN"] ) #1602 NAs !!
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.mean_LeafP"] ) #1862 NAs !!
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.log.mean_SeedMass"] ) #873 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.log.mean_SLA"] ) #1295 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.mean_LDMC"] ) #1595 NAs
+summary(mean_stability_all[mean_stability_all$lifespan_new=='not-annual',"z.mean_SSD"] ) #2618 NAs !!
+
 
 #GROWTH FORM------
 summary(as.factor(mean_stability_all$growthform)) 
@@ -97,41 +152,60 @@ mod_annuals<- lmer(z.sp_cv~ z.log.mean_height + z.mean_LeafN + z.mean_LeafP +
                    mean_stability_all[mean_stability_all$lifespan_new=='annual',], REML=F)
 # boundary (singular) fit: see ?isSingular
 summary(mod_annuals)
-coefplot(mod_annuals.1)
+coefplot(mod_annuals)
 r.squaredGLMM(mod_annuals) #not reliable
 # R2m       R2c
 # [1,] 0.3084379 0.3084379
-mod_annuals.1<- lmer(z.sp_cv~ z.mean_LeafN  +  z.mean_LDMC + 
-                     (1|Group.1) + (1|Group.2),
-                   mean_stability_all[mean_stability_all$lifespan_new=='annual'&mean_stability_all$woodyness_new=='non-woody',], REML=F)
-r.squaredGLMM(mod_annuals.1)
+                           
+
+# mod_annuals.1<- lmer(z.sp_cv~ z.mean_LeafN  +  z.mean_LDMC + 
+#                      (1|Group.1) + (1|Group.2),
+#                    mean_stability_all[mean_stability_all$lifespan_new=='annual'&mean_stability_all$woodyness_new=='non-woody',], REML=F)
+# r.squaredGLMM(mod_annuals.1)
 # R2m        R2c
 # [1,] 0.01009974 0.08823534
+
+
+mod_annuals_final<- lmer(z.sp_cv~   z.mean_LeafN + z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + 
+                     (1|Group.1) + (1|Group.2),
+                   mean_stability_all[mean_stability_all$lifespan_new=='annual',], REML=F)
+summary(mod_annuals_final)
+coefplot(mod_annuals_final)
+r.squaredGLMM(mod_annuals_final) 
+
+# ---
+
 
 mod_notannuals<- lmer(z.sp_cv~ z.log.mean_height + z.mean_LeafN + z.mean_LeafP +    
                      z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + z.mean_SSD +
                      (1|Group.1) + (1|Group.2),
-                   mean_stability_all[mean_stability_all$lifespan_new=='not-annual'&mean_stability_all$woodyness_new=='non-woody',], REML=F)
+                   mean_stability_all[mean_stability_all$lifespan_new=='not-annual',], REML=F)
 summary(mod_notannuals)
 coefplot(mod_notannuals)
 r.squaredGLMM(mod_notannuals)
 # R2m        R2c
 # [1,] 0.03330625 0.08533796
 
-mod_notannuals.1<- lmer(z.sp_cv~ z.log.mean_SeedMass + z.mean_LDMC + z.log.mean_height +
-                        (1|Group.1) + (1|Group.2),
-                      mean_stability_all[mean_stability_all$lifespan_new=='not-annual'&mean_stability_all$woodyness_new=='non-woody',], REML=F)
-summary(mod_notannuals.1)
-coefplot(mod_notannuals.1)
-r.squaredGLMM(mod_notannuals.1)
+# mod_notannuals.1<- lmer(z.sp_cv~ z.log.mean_SeedMass + z.mean_LDMC + z.log.mean_height +
+#                         (1|Group.1) + (1|Group.2),
+#                       mean_stability_all[mean_stability_all$lifespan_new=='not-annual'&mean_stability_all$woodyness_new=='non-woody',], REML=F)
+# summary(mod_notannuals.1)
+# coefplot(mod_notannuals.1)
+# r.squaredGLMM(mod_notannuals.1)
 # R2m       R2c
 # [1,] 0.03811708 0.1281322
 
+mod_notannuals_final<- lmer(z.sp_cv~  z.mean_LeafN + z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + 
+                        (1|Group.1) + (1|Group.2),
+                      mean_stability_all[mean_stability_all$lifespan_new=='not-annual',], REML=F)
+summary(mod_notannuals_final)
+coefplot(mod_notannuals_final)
+r.squaredGLMM(mod_notannuals_final)
 
 #Quantitative traits in interaction with life span----
 
 mod_interaction<- lmer(z.sp_cv~ (z.log.mean_height + z.mean_LeafN + z.mean_LeafP +    
-                        z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + z.mean_SSD)*lifespan_new +
+                        z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + z.mean_SSD)*lifespan_new -1 +
                         (1|Group.1) + (1|Group.2),
                       mean_stability_all, REML=F)
 summary(mod_interaction)
@@ -140,40 +214,66 @@ r.squaredGLMM(mod_interaction)
 # R2m       R2c
 # [1,] 0.08069282 0.1188175
 
+mod_interaction@frame$Group.1 #only 93 levels - spp for which we have all the traits!
 
-mod_interaction.1<- lmer(z.sp_cv~ ( z.mean_LeafN )*lifespan_new +  z.mean_LDMC -1 +
-                         (1|Group.1) + (1|Group.2), mean_stability_all, REML=F)
-summary(mod_interaction.1)
-coefplot(mod_interaction.1)
-r.squaredGLMM(mod_interaction.1)
-# R2m      R2c
-# [1,] 0.09444683 0.176568
-mod_interaction.2<- lmer(z.sp_cv~ lifespan_new + z.mean_LeafN  +  z.log.mean_SeedMass +  z.mean_LDMC +z.log.mean_SLA-1 +
+mod_interaction.final<-  lmer(z.sp_cv~ (z.log.mean_height + z.mean_LeafN + z.mean_LeafP +    
+                                          z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + z.mean_SSD)*lifespan_new -1 +
+                                (1|Group.1) + (1|Group.2),
+                              mean_stability_all, REML=F)
+summary(mod_interaction.final)
+coefplot(mod_interaction.final) #interaction between not-annuals and leafN keeps comming out as sign
+r.squaredGLMM(mod_interaction.final) 
+
+# annual             not-annual 
+# 606  (283 spp)     3278 (1516 spp)
+summary(z.mean_LeafN[lifespan_new=='annual']) #343 NAs
+summary(z.mean_LeafN[lifespan_new=='not-annual']) #1602 NAs 
+
+
+p<- ggplot(data = mean_stability_all[!is.na(mean_stability_all$z.sp_cv),], 
+                aes(y=z.mean_LeafN, x=lifespan_new))+
+  geom_jitter(size=0.5)+ #colour="gray", 
+  geom_violin(colour="black", position="identity", fill=NA ) +
+  # geom_point(data=est, mapping = aes(y=Value, x=Coefficient), color="red", size=3)+
+  # geom_errorbar(data=est, mapping = aes( x=Coefficient, ymin = LowOuter, ymax = HighOuter) , color="red", size=0.5, inherit.aes = F, width = 0)+
+  # geom_errorbar(data=est, mapping = aes( x=Coefficient, ymin = LowInner, ymax = HighInner), color="red", size=1.5, inherit.aes = F, width = 0)+
+  # facet_zoom(ylim = c(-0.1, 0.2), zoom.size = 1, zoom.data = z.sp_cv==0) #facet zoom on the right
+  theme_classic(base_size=15) + xlab(label = "") +ylab(label = "species CV") +  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+p
+
+# mod_interaction.1<- lmer(z.sp_cv~ ( z.mean_LeafN )*lifespan_new +  z.mean_LDMC -1 +
+#                          (1|Group.1) + (1|Group.2), mean_stability_all, REML=F)
+# summary(mod_interaction.1)
+# coefplot(mod_interaction.1)
+# r.squaredGLMM(mod_interaction.1)
+# # R2m      R2c
+# # [1,] 0.09444683 0.176568
+
+
+
+mod_additive<- lmer(z.sp_cv~ lifespan_new + z.mean_LeafN  +  z.log.mean_SeedMass +  z.mean_LDMC +z.log.mean_SLA-1 +
                            (1|Group.1) + (1|Group.2), mean_stability_all, REML=F)
-summary(mod_interaction.2)
-coefplot(mod_interaction.2)
-r.squaredGLMM(mod_interaction.2)
+summary(mod_additive)
+coefplot(mod_additive)
+r.squaredGLMM(mod_additive)
 # R2m       R2c
 # [1,] 0.08979585 0.1685861
 
-#Quant traits in addition to lifespan and growth form--------
-mod_addition<- lmer(z.sp_cv~ z.log.mean_height + z.mean_LeafN  +    
-                                   z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + 
-                                  lifespan_new + graminoid  +
-                                   (1|Group.1) + (1|Group.2),
-                       mean_stability_all, REML=F)
-summary(mod_addition) 
-coefplot(mod_addition)
-r.squaredGLMM(mod_addition)
-# R2m       R2c
-# [1,] 0.06947318 0.1157023
-levels(as.factor(mod_addition@frame$growthform)) #were are the other growthform categories???
-# [1] "graminoid"  "herb"       "herb/shrub"
-levels(as.factor(mean_stability_all$growthform)) 
-#categories dropped because of missing trait values?
+#Quant traits in addition to lifespan and graminoid--------
 
 mean_stability_all$graminoid<-'not-graminoid'
 mean_stability_all$graminoid[mean_stability_all$growthform=='graminoid']<-'graminoid'
+
+mod_addition<- lmer(z.sp_cv~ z.log.mean_height + z.mean_LeafN  +    
+                                   z.log.mean_SeedMass + z.log.mean_SLA + z.mean_LDMC + 
+                                  lifespan_new + graminoid  -1 +
+                                   (1|Group.1) + (1|Group.2),
+                       mean_stability_all, REML=F)
+summary(mod_addition) 
+coefplot(mod_addition) #graminoid category is dropped
+r.squaredGLMM(mod_addition)
+# R2m       R2c
+# [1,] 0.06947318 0.1157023
 
 
 load("dc.phylo.eig.r") 
